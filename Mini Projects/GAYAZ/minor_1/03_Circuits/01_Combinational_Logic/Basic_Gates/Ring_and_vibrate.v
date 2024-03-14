9b8b5e917bdb1ed9655647853d11906f9561b7a1
K SUPRIYA <132544420+KONETISUPRIYA@users.noreply.github.com>
2024-03-14 16:22:00 +0530

    Revert "hdlbits solutions"

--- Mini Projects/GAYAZ/minor_1/03_Circuits/01_Combinational_Logic/Basic_Gates/Ring_and_vibrate.v
+++
@@ -1,9 +0,0 @@
-module top_module (
-    input ring,
-    input vibrate_mode,
-    output ringer,       
-    output motor         
-);
-    assign motor  = vibrate_mode && ring;
-    assign ringer = (~vibrate_mode) && ring;
-endmodule
--- Mini Projects/GAYAZ/minor_1/03_Circuits/01_Combinational_Logic/K_Map_to_Circuits/4-variable_1.v
+++
@@ -1,8 +0,0 @@
-module top_module(
-    input a,
-    input b,
-    input c,
-    input d,
-    output out  ); 
-    assign out=((~a&~d)|(~a&~b&~c)|(a&~b&~c)|(c&d&b)|(c&d&a));
-endmodule
