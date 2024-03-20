9d75e2645e604ac53bff72189341dd44cde11942
ShoaibMalikSM <156204834+ShoaibMalikSM@users.noreply.github.com>
2024-03-20 23:54:16 +0530

    hdl_submissions

--- Mini Projects/SK SHOAIB/Minor_1/03 Circuits/Building Larger Circuits/3.v
+++ Mini Projects/SK SHOAIB/Minor_1/03 Circuits/Building Larger Circuits/3.v
@@ -4,7 +4,7 @@ module top_module (
     input data,
     output start_shifting);
     
-    parameter A=0, B=1, C=2, D=3, E=4;
+    parameter A=3'd0, B=3'd1, C=3'd2, D=3'd3, E=3'd4;
     reg [2:0] state,next_state;
 
     always@(posedge clk)
@@ -25,6 +25,6 @@ module top_module (
             endcase
         end
     
-    assign start_shifting = (state == E);
+    assign start_shifting = (state == E)?1'b1:1'b0;
 
 endmodule
