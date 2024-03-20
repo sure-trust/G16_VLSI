8383a1128d9714ebdfae328aaf1170dba5968c30
Sachind01 <sachintpet@gmail.com>
2024-03-20 21:43:32 +0530

    Minor Project-2

--- Mini Projects/Sachin/HDLBits/Getting started/Output zero.v
+++
@@ -1,8 +0,0 @@
-module top_module(
-    output zero
-);// Module body starts after semicolon
-
-    assign zero = 0;//no need of bit spec. for 0..
-    
-endmodule
-
--- Mini Projects/Sachin/HDLBits/Verilog Language/Basics/Simple wire.v
+++
@@ -1,8 +0,0 @@
-module top_module(in, out);
-	input in;
-	output out;
-
-	assign out = in; //blocking assgn.
-	
-endmodule
-
